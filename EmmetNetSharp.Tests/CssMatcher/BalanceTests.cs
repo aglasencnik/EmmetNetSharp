ca3891e858fba3711cb88e46805f6b02a654bf44
Amadej Glasenčnik <amadej.glasencnik@gmail.com>
2023-11-12 19:06:04 +0100

    Add unit tests for CSS Matcher Service

--- EmmetNetSharp.Tests/CssMatcher/BalanceTests.cs
+++ EmmetNetSharp.Tests/CssMatcher/BalanceTests.cs
@@ -10,12 +10,68 @@ public class BalanceTests
     [Fact]
     public void Test_Outward()
     {
+        var code = File.ReadAllText(Path.Combine("Assets", "cssMatcherSample.scss")) ?? string.Empty;
 
+        Assert.Equal(new[]
+        {
+            (145, 149),
+            (137, 150),
+            (110, 182),
+            (75, 192),
+            (61, 198),
+            (43, 281),
+            (0, 283)
+        }, _cssMatcherService.BalanceOutward(code, 140));
+
+        Assert.Equal(new[]
+        {
+            (110, 182),
+            (75, 192),
+            (61, 198),
+            (43, 281),
+            (0, 283)
+        }, _cssMatcherService.BalanceOutward(code, 77));
+
+        Assert.Equal(new[]
+        {
+            (273, 281),
+            (43, 281),
+            (0, 283)
+        }, _cssMatcherService.BalanceOutward(code, 277));
     }
 
     [Fact]
     public void Test_Inward()
     {
+        var code = File.ReadAllText(Path.Combine("Assets", "cssMatcherSample.scss")) ?? string.Empty;
 
+        Assert.Equal(new[]
+        {
+            (61, 198),
+            (75, 192),
+            (110, 182),
+            (110, 124),
+            (119, 123)
+        }, _cssMatcherService.BalanceInward(code, 62));
+
+        Assert.Equal(new[]
+        {
+            (43, 56),
+            (51, 55)
+        }, _cssMatcherService.BalanceInward(code, 46));
+
+        Assert.Equal(new[]
+        {
+            (204, 267),
+            (218, 261),
+            (218, 236),
+            (231, 235)
+        }, _cssMatcherService.BalanceInward(code, 206));
+
+        Assert.Equal(new[]
+        {
+            (330, 336),
+            (334, 335)
+        }, _cssMatcherService.BalanceInward(code, 333));
     }
 }
--- EmmetNetSharp.Tests/CssMatcher/ParserTests.cs
+++ EmmetNetSharp.Tests/CssMatcher/ParserTests.cs
@@ -7,9 +7,31 @@ public class ParserTests
 {
     private readonly ICssMatcherService _cssMatcherService = new CssMatcherService();
 
+    internal string[] Tokens(string value)
+    {
+        var splitRanges = _cssMatcherService.SplitValue(value);
+        var tokens = new List<string>();
+
+        foreach (var range in splitRanges)
+        {
+            int start = range.Item1;
+            int length = range.Item2 - start;
+            tokens.Add(value.Substring(start, length));
+        }
+
+        return tokens.ToArray();
+    }
+
     [Fact]
     public void Test_SplitValue()
     {
-
+        Assert.Equal(new[] { "10px", "20px" }, Tokens("10px 20px"));
+        Assert.Equal(new[] { "10px", "20px" }, Tokens(" 10px   20px  "));
+        Assert.Equal(new[] { "10px", "20px" }, Tokens("10px, 20px"));
+        Assert.Equal(new[] { "20px" }, Tokens("20px"));
+        Assert.Equal(new[] { "no-repeat", "10px", "5" }, Tokens("no-repeat, 10px - 5"));
+        Assert.Equal(new[] { "url(\"foo bar\")", "no-repeat" }, Tokens("url(\"foo bar\") no-repeat"));
+        Assert.Equal(new[] { "--my-prop" }, Tokens("--my-prop"));
+        Assert.Equal(new[] { "calc(100% - 80px)" }, Tokens("calc(100% - 80px)"));
     }
 }
