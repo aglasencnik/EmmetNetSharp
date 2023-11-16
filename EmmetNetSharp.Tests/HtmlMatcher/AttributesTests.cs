3aca3cb342f95b0ed3f73d63d5b4b72044afdeb8
Amadej Glasenčnik <amadej.glasencnik@gmail.com>
2023-11-16 20:59:21 +0100

    Add HTML Matcher Service unit tests

--- EmmetNetSharp.Tests/HtmlMatcher/AttributesTests.cs
+++ EmmetNetSharp.Tests/HtmlMatcher/AttributesTests.cs
@@ -1,4 +1,7 @@
-﻿namespace EmmetNetSharp.Tests.HtmlMatcher;
+﻿using EmmetNetSharp.Models;
+using Newtonsoft.Json;
+
+namespace EmmetNetSharp.Tests.HtmlMatcher;
 
 /// <summary>
 /// Represents a class that contains tests for attributes html matcher functionality.
@@ -10,6 +13,44 @@ public class AttributesTests
     [Fact]
     public void Test_ParseAttributeString()
     {
+        var resultJson = JsonConvert.SerializeObject(_htmlMatcherService.ParseAttributes("foo bar=\"baz\" *ngIf={a == b} a=b "));
+        var expectedJson = JsonConvert.SerializeObject(new[]
+        {
+            new HtmlAttributeToken
+            {
+                Name = "foo",
+                NameStart = 0,
+                NameEnd = 3
+            },
+            new HtmlAttributeToken
+            {
+                Name = "bar",
+                Value = "\"baz\"",
+                NameStart = 4,
+                NameEnd = 7,
+                ValueStart = 8,
+                ValueEnd = 13
+            },
+            new HtmlAttributeToken
+            {
+                Name = "*ngIf",
+                Value = "{a == b}",
+                NameStart = 14,
+                NameEnd = 19,
+                ValueStart = 20,
+                ValueEnd = 28
+            },
+            new HtmlAttributeToken
+            {
+                Name = "a",
+                Value = "b",
+                NameStart = 29,
+                NameEnd = 30,
+                ValueStart = 31,
+                ValueEnd = 32
+            }
+        });
 
+        Assert.Equal(expectedJson, resultJson);
     }
 }
--- EmmetNetSharp.Tests/HtmlMatcher/MatchTests.cs
+++ EmmetNetSharp.Tests/HtmlMatcher/MatchTests.cs
@@ -1,4 +1,7 @@
-﻿namespace EmmetNetSharp.Tests.HtmlMatcher;
+﻿using EmmetNetSharp.Models;
+using Newtonsoft.Json;
+
+namespace EmmetNetSharp.Tests.HtmlMatcher;
 
 /// <summary>
 /// Represents a class that contains tests for match html matcher functionality.
@@ -7,15 +10,110 @@ public class MatchTests
 {
     private readonly IHtmlMatcherService _htmlMatcherService = new HtmlMatcherService();
 
+    private readonly string _html = @"<ul>
+   <li><a href="""">text <img src=""foo.png""><link rel=""sample""> <b></b></a></li>
+   <li><a href="""">text <b></b></a></li>
+</ul>";
+
+    private readonly string _xml = @"<ul>
+   <li><a href="""">text
+       <img src=""foo.png"">
+           <link rel=""sample"" />
+       </img>
+       <b></b>
+   </a></li>
+   <li><a href="""">text <b></b></a></li>
+</ul>";
+
     [Fact]
     public void Test_Html()
     {
+        var tag1 = _htmlMatcherService.Match(_html, 12);
+        Assert.Equal("li", tag1.Name);
+        Assert.Null(tag1.Attributes);
+        Assert.Equal((9, 13), tag1.OpeningTagRange);
+        Assert.Equal((79, 84), tag1.ClosingTagRange);
 
+        // Match `<img>` tag. Since in HTML mode, it should be handled as self-closed
+        var tag2 = _htmlMatcherService.Match(_html, 37);
+        var tag2AttributesJson = JsonConvert.SerializeObject(tag2.Attributes);
+        var tag2ExpectedAttributesJson = JsonConvert.SerializeObject(new[]
+        {
+            new HtmlAttributeToken
+            {
+                Name = "src",
+                Value = "\"foo.png\"",
+                NameStart = 34,
+                NameEnd = 37,
+                ValueStart = 38,
+                ValueEnd = 47
+            }
+        });
+        Assert.Equal("img", tag2.Name);
+        Assert.Equal(tag2ExpectedAttributesJson, tag2AttributesJson);
+        Assert.Equal((29, 48), tag2.OpeningTagRange);
+        Assert.Null(tag2.ClosingTagRange);
+
+        var tag3 = _htmlMatcherService.Match(_html, 116);
+        var tag3AttributesJson = JsonConvert.SerializeObject(tag3.Attributes);
+        var tag3ExpectedAttributesJson = JsonConvert.SerializeObject(new[]
+        {
+            new HtmlAttributeToken
+            {
+                Name = "href",
+                Value = "\"\"",
+                NameStart = 96,
+                NameEnd = 100,
+                ValueStart = 101,
+                ValueEnd = 103
+            }
+        });
+        Assert.Equal("a", tag3.Name);
+        Assert.Equal(tag3ExpectedAttributesJson, tag3AttributesJson);
+        Assert.Equal((93, 104), tag3.OpeningTagRange);
+        Assert.Equal((116, 120), tag3.ClosingTagRange);
     }
 
     [Fact]
     public void Test_Xml()
     {
+        // Should match <img> tag, since we’re in XML mode, matcher should look for closing `</img>` tag
+        var tag1 = _htmlMatcherService.Match(_xml, 42, new HtmlMatcherScannerOptions { xml = true });
+        var tag1AttributesJson = JsonConvert.SerializeObject(tag1.Attributes);
+        var tag1ExpectedAttributesJson = JsonConvert.SerializeObject(new[]
+        {
+            new HtmlAttributeToken
+            {
+                Name = "src",
+                Value = "\"foo.png\"",
+                NameStart = 42,
+                NameEnd = 45,
+                ValueStart = 46,
+                ValueEnd = 55
+            }
+        });
+        Assert.Equal("img", tag1.Name);
+        Assert.Equal(tag1ExpectedAttributesJson, tag1AttributesJson);
+        Assert.Equal((37, 56), tag1.OpeningTagRange);
+        Assert.Equal((99, 105), tag1.ClosingTagRange);
 
+        var tag2 = _htmlMatcherService.Match(_xml, 70, new HtmlMatcherScannerOptions { xml = true });
+        var tag2AttributesJson = JsonConvert.SerializeObject(tag2.Attributes);
+        var tag2ExpectedAttributesJson = JsonConvert.SerializeObject(new[]
+        {
+            new HtmlAttributeToken
+            {
+                Name = "rel",
+                Value = "\"sample\"",
+                NameStart = 75,
+                NameEnd = 78,
+                ValueStart = 79,
+                ValueEnd = 87
+            }
+        });
+        Assert.Equal("link", tag2.Name);
+        Assert.Equal(tag2ExpectedAttributesJson, tag2AttributesJson);
+        Assert.Equal((69, 90), tag2.OpeningTagRange);
+        Assert.Null(tag2.ClosingTagRange);
     }
 }
