6d528a4914776f283b0acea84e53587e85a46149
Amadej Glasenčnik <amadej.glasencnik@gmail.com>
2023-12-11 13:35:31 +0100

    Add additional expand abbreviation unit tests

--- EmmetNetSharp.Tests/Abbreviation/ExpandTests.cs
+++ EmmetNetSharp.Tests/Abbreviation/ExpandTests.cs
@@ -213,7 +213,25 @@ public class ExpandTests
     [Fact]
     public void Test_FormatterOptions()
     {
+        Assert.Equal(
+            "<ul>\n\t<li class=\"item1\"></li>\n\t<li class=\"item2\"></li>\n</ul>",
+            _abbreviationService.ExpandAbbreviation("ul>.item$*2")
+        );
 
+        Assert.Equal(
+            "<ul>\n\t<li class=\"item1\"></li>\n\t<!-- /.item1 -->\n\t<li class=\"item2\"></li>\n\t<!-- /.item2 -->\n</ul>",
+            _abbreviationService.ExpandAbbreviation("ul>.item$*2", new UserConfig { Options = new AbbreviationOptions { CommentEnabled = true } })
+        );
+
+        Assert.Equal(
+            "<div>\n\t<p></p>\n</div>",
+            _abbreviationService.ExpandAbbreviation("div>p")
+        );
+
+        Assert.Equal(
+            "<div>\n\t<p>\n\t\t\n\t</p>\n</div>",
+            _abbreviationService.ExpandAbbreviation("div>p", new UserConfig { Options = new AbbreviationOptions { OutputFormatLeafNode = true } })
+        );
     }
 
     [Fact]
@@ -245,24 +263,197 @@ public class ExpandTests
     [Fact]
     public void Test_OverrideAttributes()
     {
+        var config = new UserConfig
+        {
+            Syntax = "jsx"
+        };
 
+        Assert.Equal("<div className=\"bar\"></div>", _abbreviationService.ExpandAbbreviation(".bar", config));
+        Assert.Equal("<div styleName={styles.bar}></div>", _abbreviationService.ExpandAbbreviation("..bar", config));
+        Assert.Equal("<div styleName={styles[\'foo-bar\']}></div>", _abbreviationService.ExpandAbbreviation("..foo-bar", config));
+
+        Assert.Equal("<div class=\"foo\"></div>", _abbreviationService.ExpandAbbreviation(".foo", new UserConfig { Syntax = "vue" }));
+        Assert.Equal("<div :class=\"foo\"></div>", _abbreviationService.ExpandAbbreviation("..foo", new UserConfig { Syntax = "vue" }));
     }
 
     [Fact]
     public void Test_WrapWithAbbreviation()
     {
+        Assert.Equal(
+            "<img src=\"foo.jpg\" alt=\"\"><img src=\"bar.jpg\" alt=\"\">",
+            _abbreviationService.ExpandAbbreviation("img[src=\"$#\"]*", new UserConfig { Text = new[] { "foo.jpg", "bar.jpg" } })
+        );
 
+        Assert.Equal(
+            "<div>\n\t<ul>\n\t\t<div>line1</div>\n\t\t<div>line2</div>\n\t</ul>\n</div>",
+            _abbreviationService.ExpandAbbreviation("div>ul", new UserConfig { Text = new[] { "<div>line1</div>\n<div>line2</div>" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"\">foo</a>",
+            _abbreviationService.ExpandAbbreviation("a", new UserConfig { Text = new[] { "foo" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"\">emmet//io</a>",
+            _abbreviationService.ExpandAbbreviation("a", new UserConfig { Text = new[] { "emmet//io" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"http://emmet.io\">http://emmet.io</a>",
+            _abbreviationService.ExpandAbbreviation("a", new UserConfig { Text = new[] { "http://emmet.io" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"//emmet.io\">//emmet.io</a>",
+            _abbreviationService.ExpandAbbreviation("a", new UserConfig { Text = new[] { "//emmet.io" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"http://www.emmet.io\">www.emmet.io</a>",
+            _abbreviationService.ExpandAbbreviation("a", new UserConfig { Text = new[] { "www.emmet.io" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"\">emmet.io</a>",
+            _abbreviationService.ExpandAbbreviation("a", new UserConfig { Text = new[] { "emmet.io" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"mailto:info@emmet.io\">info@emmet.io</a>",
+            _abbreviationService.ExpandAbbreviation("a", new UserConfig { Text = new[] { "info@emmet.io" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"\">uSeR@myLongDomainName.com</a>",
+            _abbreviationService.ExpandAbbreviation("a", new UserConfig { Text = new[] { "uSeR@myLongDomainName.com" } })
+        );
+
+        Assert.Equal(
+            "<p>\n\tfoo\n\tbar\n</p>",
+            _abbreviationService.ExpandAbbreviation("p", new UserConfig { Text = new[] { "foo\nbar" } })
+        );
+
+        Assert.Equal(
+            "<p>\n\t<div>foo</div>\n</p>",
+            _abbreviationService.ExpandAbbreviation("p", new UserConfig { Text = new[] { "<div>foo</div>" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"https://www.google.it\">https://www.google.it</a>",
+            _abbreviationService.ExpandAbbreviation("a", new UserConfig { Text = new[] { "https://www.google.it" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"http://www.google.it\">www.google.it</a>",
+            _abbreviationService.ExpandAbbreviation("a", new UserConfig { Text = new[] { "www.google.it" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"\">google.it</a>",
+            _abbreviationService.ExpandAbbreviation("a", new UserConfig { Text = new[] { "google.it" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"\">test here</a>",
+            _abbreviationService.ExpandAbbreviation("a", new UserConfig { Text = new[] { "test here" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"mailto:test@domain.com\">test@domain.com</a>",
+            _abbreviationService.ExpandAbbreviation("a", new UserConfig { Text = new[] { "test@domain.com" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"\">test here test@domain.com</a>",
+            _abbreviationService.ExpandAbbreviation("a", new UserConfig { Text = new[] { "test here test@domain.com" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"\">test here www.domain.com</a>",
+            _abbreviationService.ExpandAbbreviation("a", new UserConfig { Text = new[] { "test here www.domain.com" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"https://www.google.it\">https://www.google.it</a>",
+            _abbreviationService.ExpandAbbreviation("a[href=]", new UserConfig { Text = new[] { "https://www.google.it" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"http://www.google.it\">www.google.it</a>",
+            _abbreviationService.ExpandAbbreviation("a[href=]", new UserConfig { Text = new[] { "www.google.it" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"\">google.it</a>",
+            _abbreviationService.ExpandAbbreviation("a[href=]", new UserConfig { Text = new[] { "google.it" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"\">test here</a>",
+            _abbreviationService.ExpandAbbreviation("a[href=]", new UserConfig { Text = new[] { "test here" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"mailto:test@domain.com\">test@domain.com</a>",
+            _abbreviationService.ExpandAbbreviation("a[href=]", new UserConfig { Text = new[] { "test@domain.com" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"\">test here test@domain.com</a>",
+            _abbreviationService.ExpandAbbreviation("a[href=]", new UserConfig { Text = new[] { "test here test@domain.com" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"\">test here www.domain.com</a>",
+            _abbreviationService.ExpandAbbreviation("a[href=]", new UserConfig { Text = new[] { "test here www.domain.com" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"mailto:test@domain.com\" class=\"here\">test@domain.com</a>",
+            _abbreviationService.ExpandAbbreviation("a[class=here]", new UserConfig { Text = new[] { "test@domain.com" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"http://www.domain.com\" class=\"here\">www.domain.com</a>",
+            _abbreviationService.ExpandAbbreviation("a.here", new UserConfig { Text = new[] { "www.domain.com" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"\" class=\"here\">test here test@domain.com</a>",
+            _abbreviationService.ExpandAbbreviation("a[class=here]", new UserConfig { Text = new[] { "test here test@domain.com" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"\" class=\"here\">test here www.domain.com</a>",
+            _abbreviationService.ExpandAbbreviation("a.here", new UserConfig { Text = new[] { "test here www.domain.com" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"www.google.it\">test</a>",
+            _abbreviationService.ExpandAbbreviation("a[href=\"www.google.it\"]", new UserConfig { Text = new[] { "test" } })
+        );
+
+        Assert.Equal(
+            "<a href=\"www.example.com\">www.google.it</a>",
+            _abbreviationService.ExpandAbbreviation("a[href=\"www.example.com\"]", new UserConfig { Text = new[] { "www.google.it" } })
+        );
     }
 
     [Fact]
     public void Test_ClassNames()
     {
-
+        Assert.Equal("<div class=\"foo\">", _abbreviationService.ExpandAbbreviation("div.foo/"));
+        Assert.Equal("<div class=\"foo1/2\"></div>", _abbreviationService.ExpandAbbreviation("div.foo1/2"));
+        Assert.Equal("<div class=\"foo 1/2\"></div>", _abbreviationService.ExpandAbbreviation("div.foo.1/2"));
     }
 
     [Fact]
     public void Test_Pug_Basics()
     {
-
+        Assert.Equal(
+            "doctype html\nhtml(lang=\"en\")\n\thead\n\t\tmeta(charset=\"UTF-8\")\n\t\tmeta(name=\"viewport\", content=\"width=device-width, initial-scale=1.0\")\n\t\ttitle Document\n\tbody ",
+            _abbreviationService.ExpandAbbreviation("!", new UserConfig { Syntax = "pug" })
+        );
     }
 }
